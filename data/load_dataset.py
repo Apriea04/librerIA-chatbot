02a44be23e4130399ffeca5bdbf65adf64843b8b
Álvaro Prieto Álvarez <apriea04@estudiantes.unileon.es>
2024-10-18 15:32:56 +0200

    Fix or supress warnings

--- data/load_dataset.py
+++ data/load_dataset.py
@@ -1,7 +1,6 @@
 from neo4j import ManagedTransaction
 import pandas as pd
 from utils import db
-
 import ast
 import os
 import dotenv
@@ -9,7 +8,7 @@ import dotenv
 dotenv.load_dotenv()
 BOOKS_PATH = os.getenv('BOOKS_PATH')
 RATINGS_PATH = os.getenv('RATINGS_PATH')
-BATCH_SIZE = int(os.getenv("BATCH_SIZE"))
+BATCH_SIZE = int(os.getenv("BATCH_SIZE")) # type: ignore
 
 def create_author_nodes(tx: ManagedTransaction, df: pd.DataFrame):
     """
@@ -25,18 +24,18 @@ def create_author_nodes(tx: ManagedTransaction, df: pd.DataFrame):
     print(f"Procesando {total_rows} filas en bloques de {BATCH_SIZE}...")
 
     # Procesar fila por fila
-    for idx, authors_str in enumerate(df['authors']):
+    for idx, authors_str in enumerate(df['authors'].astype(str)):
         if pd.notna(authors_str):
             try:
                 authors_list = ast.literal_eval(authors_str)
                 if isinstance(authors_list, list):
-                    all_authors.extend(authors_list)
+                    all_authors.extend(authors_list) # type: ignore
             except (ValueError, SyntaxError):
                 continue
 
         # Ejecutar la query cada vez que alcancemos un múltiplo del tamaño del bloque (5000 filas)
         if (idx + 1) % BATCH_SIZE == 0 or (idx + 1) == total_rows:
-            unique_authors = list(set(all_authors))  # Remover duplicados dentro del bloque
+            unique_authors: list[str] = list(set(all_authors)) # type: ignore # Remover duplicados dentro del bloque
             print(f"Procesando bloque de filas {idx + 1 - BATCH_SIZE + 1} a {idx + 1} (autores únicos en este bloque: {len(unique_authors)})")
             
             if unique_authors:  # Solo ejecutar la query si hay autores en el bloque
@@ -66,7 +65,7 @@ def main():
         "Relaciones creadas" after relationships are created.
     """
     # Cargar el dataset
-    df = pd.read_csv(BOOKS_PATH)
+    df: pd.DataFrame = pd.read_csv(BOOKS_PATH) # type: ignore
     
     # Crear nodos
     with db.connect() as driver:
