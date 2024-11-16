e9738044e547ecc7bbcace0f744354d40bfdc05d
Álvaro Prieto Álvarez <apriea04@estudiantes.unileon.es>
2024-11-16 13:31:16 +0100

    Refactor: eliminar función de carga de datos optimizada y simplificar el flujo en main.py

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
 import time
 
-import utils.load_dataset_optimized as load_dataset
 start_time = time.time()
 
-#parallel_data.main()
-load_dataset.load_dataset()
 
 end_time = time.time()
 elapsed_time = end_time - start_time
--- utils/load_dataset_optimized.py
+++
@@ -1,9 +0,0 @@
-import utils.db as db
-
-
-def load_dataset():
-    with db.connect() as driver:
-        with driver.session() as session:
-            with open("utils/p.cypher", "r") as file:
-                query = file.read()
-            session.run(query)  # type: ignore
