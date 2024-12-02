8d27c63c542ebd93e9b57061ef5eeb042a669cfe
Álvaro Prieto Álvarez <apriea04@estudiantes.unileon.es>
2024-12-02 18:53:08 +0100

    Feat: agregar método para recomendar libros del mismo género basado en el título del libro

--- agents/rag_agent.py
+++ agents/rag_agent.py
@@ -1,11 +1,17 @@
 from utils.db import connect
 from models.embedding_manager import EmbeddingManager
 
+
 class RAGAgent:
     def __init__(self):
         self.neo4j_conn = connect()
 
-    def recommend_similar_books_by_title(self, book_title: str, top_k: int=5, embedding_property: str="title_embedding") -> list:
+    def recommend_similar_books_by_title(
+        self,
+        book_title: str,
+        top_k: int = 5,
+        embedding_property: str = "title_embedding",
+    ) -> list:
         """
         Recommends similar books based on the title of a given book.
         This method uses the Neo4j graph database to find books that have similar embeddings
@@ -20,18 +26,20 @@ class RAGAgent:
             list: A list of tuples, where each tuple contains the title of a similar book and
                   the similarity score.
         """
-    
+
         with self.neo4j_conn.session() as session:
             # Obtener el embedding del libro especificado
             query = f"""
             MATCH (b:Book {{title: $title}})
             RETURN b.{embedding_property} AS embedding
             """
-            result = session.run(query, {"title": book_title}).single() # type: ignore
-            book_embedding = result['embedding'] if result else None
+            result = session.run(query, {"title": book_title}).single()  # type: ignore
+            book_embedding = result["embedding"] if result else None
 
             if book_embedding is None:
-                book_embedding = EmbeddingManager().generate_text_embedding([book_title])[0]
+                book_embedding = EmbeddingManager().generate_text_embedding(
+                    [book_title]
+                )[0]
 
             # Consulta para encontrar los libros más similares
             similar_books_query = f"""
@@ -42,11 +50,16 @@ class RAGAgent:
             ORDER BY similarity DESC
             LIMIT $top_k
             """
-            similar_books = session.run(similar_books_query, {"embedding": book_embedding, "top_k": top_k, "title": book_title}) # type: ignore
+            similar_books = session.run(similar_books_query, {"embedding": book_embedding, "top_k": top_k, "title": book_title})  # type: ignore
 
-            return [(record['title'], record['similarity']) for record in similar_books]
+            return [(record["title"], record["similarity"]) for record in similar_books]
 
-    def recommend_similar_books_by_description(self, book_description: str, top_k: int=5, embedding_property: str="description_embedding") -> list:
+    def recommend_similar_books_by_description(
+        self,
+        book_description: str,
+        top_k: int = 5,
+        embedding_property: str = "description_embedding",
+    ) -> list:
         """
         Recommends similar books based on the given description.
         This method uses the Neo4j graph database to find books that have similar embeddings
@@ -60,9 +73,11 @@ class RAGAgent:
             list: A list of tuples, where each tuple contains the title of a similar book and
                   the similarity score.
         """
-    
-        descr_embedding = EmbeddingManager().generate_text_embedding([book_description])[0]
-        
+
+        descr_embedding = EmbeddingManager().generate_text_embedding(
+            [book_description]
+        )[0]
+
         with self.neo4j_conn.session() as session:
             # Consulta para encontrar los libros más similares
             similar_books_query = f"""
@@ -73,7 +88,63 @@ class RAGAgent:
             ORDER BY similarity DESC
             LIMIT $top_k
             """
-            similar_books = session.run(similar_books_query, {"embedding": descr_embedding, "top_k": top_k}) # type: ignore
-            
-            return [(record['title'], record['similarity']) for record in similar_books]
-            
+            similar_books = session.run(similar_books_query, {"embedding": descr_embedding, "top_k": top_k})  # type: ignore
+
+            return [(record["title"], record["similarity"]) for record in similar_books]
+
+    def recommend_same_genre_as(
+        self,
+        book_title: str,
+        top_k: int = 5,
+        description_embedding_property: str = "description_embedding",
+    ) -> list:
+        """
+        Recommends books of the same genre as the specified book title.
+        If the book has a description embedding, it uses that to find similar books belonging to the same genre.
+        Otherwise, it just finds books with the same genre.
+        Args:
+            book_title (str): The title of the book for which to find similar genre books.
+            top_k (int, optional): The number of similar genre books to return. Defaults to 5.
+            description_embedding_property (str, optional): The property name of the book node that contains
+                                                            the description embedding. Defaults to "description_embedding".
+        Returns:
+            list: A list of tuples, where each tuple contains the title of a similar genre book and
+                  the similarity score.
+        """
+        book_query = f"""
+        MATCH (b:Book {{title: $title}})-[:BELONGS_TO]->(g:Genre)
+        RETURN g.name AS genre, b.{description_embedding_property} AS embedding
+        """
+        with self.neo4j_conn.session() as session:
+            result = session.run(book_query, {"title": book_title}).single() # type: ignore
+
+            if result is None:
+                return []
+
+            genre = result["genre"]
+
+            if result["embedding"] is not None:
+                book_embedding = result["embedding"]
+                similar_books_query = f"""
+                MATCH (b:Book)-[:BELONGS_TO]->(g:Genre {{name: $genre}})
+                WHERE b.title <> $title AND b.{description_embedding_property} IS NOT NULL
+                WITH b, gds.similarity.cosine(b.{description_embedding_property}, $embedding) AS similarity
+                RETURN b.title AS title, similarity
+                ORDER BY similarity DESC
+                LIMIT $top_k
+                """
+                similar_books = session.run(similar_books_query, {"embedding": book_embedding, "top_k": top_k, "title": book_title, "genre": genre})  # type: ignore
+
+            else:
+                similar_books_query = f"""
+                MATCH (b:Book)-[:BELONGS_TO]->(g:Genre {{name: $genre}})
+                WHERE b.title <> $title
+                RETURN b.title AS title
+                LIMIT $top_k
+                """
+                similar_books = session.run(
+                    similar_books_query,
+                    {"top_k": top_k, "title": book_title, "genre": genre},
+                )
+
+            return [(record["title"], record["similarity"]) for record in similar_books]
--- main.py
+++ main.py
@@ -25,7 +25,12 @@ print('-'*106)
 results = ragAgent.recommend_similar_books_by_description("a man of God who is busy caring for his quadriplegic wife, severely injured in a serious car accident. In an innocent effort to reach out to a lonely member of his church, Heath finds himself as the man and not the minister when Heath and Julia give their bodies to each other and face God's wrath. Julia is overtaken by a life-threatening illness, the loss of her home and rumors of his wicked affair.", 10) # Expected Whispers of the Wicked Saints
 for idx, result in enumerate(results, start=1):
     print(f"{idx}. {result[0]} (Score: {result[1]:.8f})")
+    
+print('-'*106)
 
+results = ragAgent.recommend_same_genre_as("The Human Zoo", 12) # Expected Animal Behavior genre: 	How Animals Talk: And Other Pleasant Studies of Birds and Beasts, 	Never Cry Wolf,	Cry Wolf, 	Can I Be Good?, Call of the Dolphins, ...
+for idx, result in enumerate(results, start=1):
+    print(f"{idx}. {result[0]} (Score: {result[1]:.8f})")
 
 end_time = time.time()
 elapsed_time = end_time - start_time
