c3833c39b12c032876694bdeb564f6c7e5c0d2d0
Álvaro Prieto Álvarez <apriea04@estudiantes.unileon.es>
2024-12-22 17:42:18 +0100

    Feat: probar herramienta de recomendación de libros como prueba de concepto

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import time
 from utils.db_manager import DBManager
 from utils.env_loader import EnvLoader
 from agents import rag_tools
+from agents.agent import Agent
 
 start_time = time.time()
 
@@ -10,11 +11,11 @@ env = EnvLoader()
 dbManager = DBManager()
 
 # Generate embeddings for the dataset:
-#dbManager.generate_embeddings_for("Book", "title", "title", env.embeddings_model, 32)
-#dbManager.generate_embeddings_for("Book", "description", "title", env.embeddings_model, 32)
-#dbManager.generate_embeddings_for("Review", "summary", "", env.embeddings_model, 32)
-#dbManager.generate_embeddings_for("Review", "text", "", env.embeddings_model, 1)
-
+# dbManager.generate_embeddings_for("Book", "title", "title", env.embeddings_model, 32)
+# dbManager.generate_embeddings_for("Book", "description", "title", env.embeddings_model, 32)
+# dbManager.generate_embeddings_for("Review", "summary", "", env.embeddings_model, 32)
+# dbManager.generate_embeddings_for("Review", "text", "", env.embeddings_model, 1)
+"""
 results = rag_tools.recommend_similar_books_by_title("Santa Biblia", 10)
 for idx, result in enumerate(results, start=1):
     print(f"{idx}. {result[0]} (Score: {result[1]:.8f})")
@@ -41,3 +42,48 @@ end_time = time.time()
 elapsed_time = end_time - start_time
 minutes, seconds = divmod(elapsed_time, 60)
 print(f"Execution time: {int(minutes)} minutes and {seconds:.2f} seconds")
+"""
+import random
+from langchain_core.tools import tool
+@tool
+def weather_forecast(location: str):
+    """Weather forecast tool."""
+    tmp = random.randint(-10, 35)
+    print(f"Weather for {location}: {tmp}")
+    return f"{tmp}ºC, sunny"
+
+@tool
+def recommend_similar_books_by_title(title: str, top_k: int=5):
+    """
+    Recommends similar books based on the title of a given book.
+    This method uses the Neo4j graph database to find books that have similar embeddings
+    to the specified book title.
+    Args:
+        book_title (str): The title of the book for which to find similar books.
+        top_k (int, optional): The number of similar books to return. Defaults to 5.
+    Returns:
+        list: A list of tuples, where each tuple contains the title of a similar book and
+              the similarity score.
+    """
+    val = str(rag_tools.recommend_similar_books_by_title(title, top_k))
+    print(f"Recommend similar books by title: {val}")
+    return val
+
+agent = Agent(
+    model_name=env.agent_llm_model,
+    prompt_path="agents/prompts/REACT_agent.txt",
+    tools=[
+        weather_forecast,
+        recommend_similar_books_by_title,
+        #rag_tools.recommend_similar_books_by_description,
+        #rag_tools.recommend_same_genre_as,
+        #rag_tools.recommend_same_author_as,
+    ],
+)
+
+while True:
+    message = input("You: ")
+    if message.lower() == "exit":
+        break
+    response = agent.send_msg(message)
+    print(f"Bot: {response}")
